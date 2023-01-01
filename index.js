0b082a157aa3279c0027678c3869f4891ab0a9bf
Raja S <39332761+raja1205@users.noreply.github.com>
2023-01-01 22:57:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
 // Add Express
 const express = require("express");
+const dotenv = require("dotenv").config();
 
 // Initialize Express
 const app = express();
+const cors = require("cors");
+
+const MongoClient = require("mongodb").MongoClient;
+const client = new MongoClient(process.env.DB_URL);
+
+//Get Projects data
+const getProjectsData = 
 
 // Create GET request
 app.get("/", (req, res) => {
-  res.send("Express on Vercel");
+  res.send("Node Server is Running...");
+});
+
+app.get("/api/v1/projectsdata", async function (req, res) {
+    try {
+        const connection = await client.connect();
+        const db = connection.db("portfolio");
+
+		const projectsData = await db.collection("projects").find({}).toArray();
+        const skillsData = await db.collection("skills").find({}).toArray();
+        const achievementsData = await db.collection("achievements").find({}).toArray()
+		
+		let portfolioData = [];
+		portfolioData.push(projectsData, skillsData, achievementsData);
+
+		res.json(portfolioData);
+        await connection.close();
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 // Initialize server
-app.listen(5000, () => {
-  console.log("Running on port 5000.");
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Node Server is listening at 5000`);
 });
 
 module.exports = app;
